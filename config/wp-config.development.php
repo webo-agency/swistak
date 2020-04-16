1dac48fad5d7f376c94f072928db0fcc593c1952
Triloworld <patryk@padus.pl>
2020-04-16 16:11:32 +0200

    No local set

--- config/wp-config.development.php
+++ config/wp-config.development.php
@@ -8,17 +8,63 @@
  */
   
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** MySQL hostname */
-define('DB_HOST', '');
+// 
+// FROM PRODUCTION SET - REPLACE WITH LOCAL WERSION
+//
 
+define('WP_CACHE', false);
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '');
+define( 'DB_NAME', '' );
 
 /** MySQL database username */
-define('DB_USER', '');
+define( 'DB_USER', '' );
 
-/** MySQL database password - set in wp-config.local.php */
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
+
+/**#@-*/
+
 
 /**
  * For developers: WordPress debugging mode.
@@ -33,3 +79,6 @@ define('DB_USER', '');
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', true);
+
+
+
