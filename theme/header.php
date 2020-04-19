dcfd41549a2a2a80e1e62a70d7240caed40557c2
Tomek A <tomasz_adamczyk@hotmail.com>
2020-04-19 13:02:15 +0200

    import google font

--- theme/header.php
+++ theme/header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
 
 	<?php wp_head(); ?>
 </head>
