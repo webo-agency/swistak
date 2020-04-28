5e90666e1b60123401e3da1af67a64c6c368b233
Triloworld <patryk@padus.pl>
2020-04-28 11:17:02 +0200

    Naprawa builda

--- theme/assets/js/script.js
+++ theme/assets/js/script.js
@@ -1,7 +1,7 @@
 import WavesStore from './WaveDecoration/WaveStore/WaveStore';
 import { caseStudiesSwiper }  from './SwiperCarousels/caseStudiesSwiper/caseStudiesSwiper';
 import { caseStudiesSwiperIndexCounter }  from './SwiperCarousels/caseStudiesSwiper/caseStudiesSwiperIndexCounter';
-import { recommendationsSwiper }  from './SwiperCarousels/recommendationSwiper/recommendationSwiper';
+import { recommendationsSwiper }  from './SwiperCarousels/RecommendationSwiper/recommendationSwiper';
 import { contentSwiper } from './SwiperCarousels/ContentSwiper/contentSwiper';
 
 caseStudiesSwiperIndexCounter.initSwiper();
