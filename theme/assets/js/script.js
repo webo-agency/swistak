a40f4c4da6f91c8e3fe79b97aec81ada20fa15f1
Tomek A <tomasz_adamczyk@hotmail.com>
2020-04-29 17:14:49 +0200

    video class instance init

--- theme/assets/js/script.js
+++ theme/assets/js/script.js
@@ -3,7 +3,12 @@ import { caseStudiesSwiper }  from './SwiperCarousels/caseStudiesSwiper/caseStud
 import { caseStudiesSwiperIndexCounter }  from './SwiperCarousels/caseStudiesSwiper/caseStudiesSwiperIndexCounter';
 import { recommendationsSwiper }  from './SwiperCarousels/RecommendationSwiper/recommendationSwiper';
 import { contentSwiper } from './SwiperCarousels/ContentSwiper/contentSwiper';
+import VideoHandler from './Video/VideoHandler';
 
 caseStudiesSwiperIndexCounter.initSwiper();
+
 const wavesStore = new WavesStore();
 wavesStore.render();
+
+const video = new VideoHandler();
+video.init();
