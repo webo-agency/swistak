fa3fff521623560f809a9c48d66d18708971634e
Tomek A <tomasz_adamczyk@hotmail.com>
2020-05-01 14:11:22 +0200

    transform removed

--- theme/assets/js/HeaderPosition/HeaderPosition.js
+++ theme/assets/js/HeaderPosition/HeaderPosition.js
@@ -1,19 +1,12 @@
 class HeaderPosition {
     constructor() {
         this.header = document.querySelector('.ks-header');
-        this.translateYVisible = 'translateY(0)';
-        this.translateYHidden = 'translateY(-100%)';
         this.fixedClassname = 'ks-header__fixed';
         this.lastScrollTop = 0;
     }
 
-    headerTranslateHide() {
-        this.header.style.transform = this.translateYHidden;
-    }
-
     showHeader() {
         this.header.classList.add(this.fixedClassname)
-        this.header.style.transform = this.translateYVisible;
     }
 
     hideHeader() {
@@ -26,7 +19,6 @@ class HeaderPosition {
             if(window.scrollY > this.header.clientHeight) {
                 if (st > this.lastScrollTop){
                     this.hideHeader();
-                    this.headerTranslateHide();
                 } else {
                     this.showHeader();
                 }
