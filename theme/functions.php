af138f8226d5c71ca48ce3bde62f1c3b17801609
Tomek A <tomasz_adamczyk@hotmail.com>
2020-07-11 18:17:05 +0200

    SWISTAK-27 changed css source main file directory

--- theme/functions.php
+++ theme/functions.php
@@ -182,7 +182,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 function swistak_theme_styles_and_scripts() {
-	wp_register_style('custom-styles', get_template_directory_uri().'/assets/public/dist/css/style.min.css');
+	wp_register_style('custom-styles', get_template_directory_uri().'/assets/public/dist/css/style.css');
 	wp_enqueue_style('custom-styles');
 	wp_enqueue_script( 'swiper', get_template_directory_uri() . '/assets/js/swiper.min.js', array (), 1.5, true);
 	wp_enqueue_script( 'anchor-scroll', get_template_directory_uri() . '/assets/js/anchor-scroll-master/scroll.min.js', array (), 1.5, true);
