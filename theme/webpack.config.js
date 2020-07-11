708e3d48ba762d99253b0159be8fc9aa5b8365b6
Tomek A <tomasz_adamczyk@hotmail.com>
2020-07-11 18:16:31 +0200

    SWISTAK-27 webpack scss and files configuration added

--- theme/webpack.config.js
+++ theme/webpack.config.js
@@ -1,26 +1,94 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: {main: './assets/js/script.js'},
+const jsConfig = {
+    entry: {
+        main: ['./assets/js/script.js'],
+    },
     output: {
         path: path.resolve(__dirname, 'assets/public/dist/js'),
         filename: 'script.bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                    plugins: ['@babel/transform-runtime']
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/transform-runtime']
+                    }
                 }
-            }
-        }]
+            },
+        ]
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'assets'),
         publicPath: '/public/dist/js/'
-      }
+    }
 }
+
+const scssConfig = {
+    entry: {
+        style: ['./assets/sass/style.scss'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets/public/dist/css'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'images',
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            sourceMap: true,
+                            plugins: [
+                                require('autoprefixer'),
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            implementation: require('dart-sass'),
+                            sassOptions: {
+                                fiber: false,
+                                sourceMap: true,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+}
+
+module.exports = [jsConfig, scssConfig];
