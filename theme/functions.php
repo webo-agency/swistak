82a933c4628280203b685fabfb1a28f375970cb1
Tomek A <tomasz_adamczyk@hotmail.com>
2020-07-13 21:29:07 +0200

    SWISTAK-26 modernizr.js included into functions.php

--- theme/functions.php
+++ theme/functions.php
@@ -184,6 +184,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 function swistak_theme_styles_and_scripts() {
 	wp_register_style('custom-styles', get_template_directory_uri().'/assets/public/dist/css/style.css');
 	wp_enqueue_style('custom-styles');
+	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/modernizr.js', array (), 1.5, true);
 	wp_enqueue_script( 'swiper', get_template_directory_uri() . '/assets/js/swiper.min.js', array (), 1.5, true);
 	wp_enqueue_script( 'anchor-scroll', get_template_directory_uri() . '/assets/js/anchor-scroll-master/scroll.min.js', array (), 1.5, true);
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/public/dist/js/script.bundle.js', array('swiper', 'anchor-scroll'), 1.5, true);
