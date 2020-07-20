6de64e5e00ff544643a1439b1de27821d8279cfc
Tomek A <tomasz_adamczyk@hotmail.com>
2020-07-20 18:07:02 +0200

    SWISTAK-28 deleted unnecessary swiper files

--- theme/assets/js/SwiperCarousels/ContentSwiper/contentSwiper.js
+++
@@ -1,9 +0,0 @@
-
-export const contentSwiper = new Swiper('.ks-content__swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    pagination: {
-        el: '.ks-content__swiper-pagination',
-        clickable: true,
-    },
-});
